9aa334eeb490ebe4d822d666440aa883466d73a2
rommytech <rommybejar@gmail.com>
2024-10-19 16:02:17 -0300

    Add files via upload

---
+++ Proyecto1/ejemplo.js
@@ -0,0 +1,25 @@
+//Forma 1
+var nombre = "Juan"; //Variable String
+//Forma 2
+apellido = "López"; //Variable String
+//Forma 3
+var direccion; //Variable sin tipo
+//Forma 4 . Mejora
+let telefono; 
+//Forma 5 - Mejora
+let email = 'me@google.com'; //Declarar variable String
+
+//let email = 'yo@hotmail.com'; //No se permite let dos veces en una misma var.
+const gravedad = 9.8;
+
+var nombreCom = "Donnut's";
+
+/* Numeros */
+var edad = 30; // Variable tipo Integer
+nota = 7; // Variable tipo Integer
+let cantidad = 10; // Variable tipo Integer
+peso = 56.45 // Variable tipo Float
+
+//Booleans
+var esdedia = false; //Variable boolean
+esdenoche = true; // Variable boolean 
